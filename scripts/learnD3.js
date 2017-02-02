92d6db63563f4b56bb57ae093250410ba091f4c9
JHenderson219 <hendersonj219@gmail.com>
2017-02-01 17:32:04 -0800

    Complete labled scatter plot

--- scripts/learnD3.js
+++ scripts/learnD3.js
@@ -1,6 +1,6 @@
-const dataset = [12, 31, 22, 17, 25, 18, 29, 14, 9];
+/*const dataset = [12, 31, 22, 17, 25, 18, 29, 14, 9];
 const w = 500;
-const h = 200;
+const h = 500;
 
 const svg = d3.select('body')
 	.append("svg")	
@@ -15,18 +15,56 @@ const svg = d3.select('body')
 	.attr("height", (d) => d*3)
 	.attr('x', (d,i) => i*30)
 	.attr('y', (d) => h-d*3)
-	.attr ('fill', "navy");
+	.attr ('fill', "navy")
+	.attr("class","bar")
+	.append('title')
+	.text((d) => d);
 
 	svg.selectAll('text')
 		.data(dataset)
 		.enter()
 		.append('text')
 		.attr('x', (d,i) => i*30)
-		.attr('y', (d) => h-d*3-3)
-		.text(d => d)
-		.style("font-size","25px")
-		.style("fill", "red");
-
+		.attr('y', (d) => h-d*3+3);
 		
 
+    */
+
+const dataset = [
+	[ 34,    78 ],
+	[ 109,   280 ],
+	[ 310,   120 ],
+	[ 79,    411 ],
+	[ 420,   220 ],
+	[ 233,   145 ],
+	[ 333,   96 ],
+	[ 222,   333 ],
+	[ 78,    320 ],
+	[ 21,    123 ]
+    ];
     
+    
+const w = 500;
+const h = 500;
+    
+const svg = d3.select("body")
+              .append("svg")
+              .attr("width", w)
+              .attr("height", h);
+    
+svg.selectAll("circle")
+	.data(dataset)
+	.enter()
+	.append("circle")
+    .attr("cx", (d, i) => d[0])
+    .attr("cy", (d, i) => h - d[1])
+    .attr("r", 5);
+
+svg.selectAll("text")
+	.data(dataset)
+	.enter()
+	.append("text")
+	.text((d, i) => d[0]+", "+d[1])
+	.attr("x", (d, i) => d[0]+5)
+    .attr("y", (d, i) => h - d[1]);
+	
